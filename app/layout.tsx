5d67721af0d53fe6fa15c4246b380eaa5e035769
Rajdip Pramanik <rajdippramanik31@gmail.com>
2025-01-02 19:12:40 +0530

    Modal need to be placed properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import OuterLayer from "@/components/outerlayer";
 import { ThemeProvider } from "@/components/theme-provider";
+import Modal from "../components/Modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
